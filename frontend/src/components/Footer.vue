fe7da993fdba40fe3352a01e1381d162a1c70e27
Sigmond Kukla <picoplanetdev@gmail.com>
2023-10-16 16:42:19 -0400

    Improved footer style

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="pt-5 my-5 text-muted border-top">
+    <footer class="py-4 text-muted border-top">
         Created by Sigmond Kukla &middot; &copy; {{ new Date().getFullYear() }}
     </footer>
 </template>
