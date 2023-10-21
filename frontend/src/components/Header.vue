f7d334243c6aec3ec06a41955e3cc723252a5167
Sigmond Kukla <picoplanetdev@gmail.com>
2023-10-21 09:50:13 -0400

    Fixed favicon issue in build

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,13 +5,13 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
     <header class="d-flex align-items-center pb-3 mb-5 border-bottom">
         <RouterLink to="/" class="fs-4 text-body text-decoration-none">
-            <img src="favicon/favicon.svg" width="40" class="me-2" alt="Whiteboard recorder logo">
+            <img src="/favicon/favicon.svg" width="40" class="me-2" alt="Whiteboard recorder logo">
             <span class="fs-4">Whiteboard Recorder</span>
         </RouterLink>
         <RouterLink to="/" class="ms-auto fs-4 text-body text-decoration-none me-5">
-                <i class="bi bi-house-fill"></i>
-                Home
-            </RouterLink>
+            <i class="bi bi-house-fill"></i>
+            Home
+        </RouterLink>
         <RouterLink to="/settings" class="fs-4 text-body text-decoration-none">
             <i class="bi bi-gear-fill"></i>
             Settings
--- frontend/src/components/SettingsForm.vue
+++ frontend/src/components/SettingsForm.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
-</script >
+</script>
 
 <template>
     <form>
